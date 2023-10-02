ba64219dd9d19ba17a74867db21d4a10e95cdbf0
Your Name <you@example.com>
2023-10-02 22:08:13 +0545

    responsive design

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,6 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel'
-import img1 from '../assets/img1.jpg'
-import img2 from '../assets/img2.jpg'
-import img3 from '../assets/img3.jpg'
+import {AiOutlineSearch} from 'react-icons/ai'
 
 const Home = () => {
   const styles = {
@@ -12,35 +10,38 @@ const Home = () => {
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    marginTop:"10px",
-    objectFit:"cover"
-    
+    marginTop: "10px",
+    objectFit: "cover"
+
   }
   return (
     <>
-    <Carousel className="mt-5" 
-    showThumbs={false}
-    swipeable={true}
-    >
-      <div className={`bg-img-1 h-[90vh] bg-cover bg-no-repeat`}>
-        <p>Plan your vacation with us..</p>
-        <input type="text" /> 
-        <button>Search</button>
-        {/* <img src={img1}  style={styles} width={1350}/> */}
-      </div>
+      <Carousel className="mt-9"
+        showThumbs={false}
+        swipeable={true}
+      >
+        <div className={`bg-img-1 h-[90vh] bg-cover bg-no-repeat relative`}>
+          <p className="para font-serif">Jobs <span className="text-blue-900">fill </span> your pocket, <br /> but <span className="text-blue-900"> adventures</span>  fills <br /> your soul</p>
+          <div className="search">
+            <input type="text"  className="font-serif text-xs" />
+            <button className=""><AiOutlineSearch/></button>
+          </div>
+
+          {/* <img src={img1}  style={styles} width={1350}/> */}
+        </div>
         <div className={`bg-img-2 h-[90vh] bg-cover`}>
-            <p>Explore the world rediscover yourself..</p>
+          <p>Explore the world rediscover yourself..</p>
         </div>
         <div className={`bg-img-3 h-[90vh] bg-cover`}>
           <p>To Travel is to live..</p>
           {/* <img src={img3}  style={styles} width={1350}/> */}
         </div>
-      
-        
 
-    </Carousel>
-   
-  </>
+
+
+      </Carousel>
+
+    </>
   )
 }
 
--- src/component/Availablepackage.jsx
+++ src/component/Availablepackage.jsx
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom'
 
 const Availablepackage = () => {
     return (
-        <div className="contains  mt-12 grid grid-cols-4 gap-1">
+        <div className="contains  mt-12 grid grid-cols-1  lg:grid lg:grid-cols-4 lg:gap-1">
             {/* <h1 className=' mt-5 text-2xl bold font-mono justify-center text-center text-blue-800'>Special Packages</h1> */}
             <div className='container relative left-3 right-3  mb-2 rounded-md border-2 border-black h-30 '>
                 <img src={img1} alt="img" className='p-1' />
